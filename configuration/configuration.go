ef821b626b25e6159f2f8bb4c3f68567b9d8e93a
Maria Campana <maria.campana@beamery.com>
2018-10-19 14:53:14 +0100

    Make it work with directories

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -3,6 +3,7 @@ package configuration
 import (
 	"io/ioutil"
 	"log"
+	"os"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -14,7 +15,33 @@ type GeneralConfig struct {
 	Spec       interface{} `yaml:"Spec"`
 }
 
-func NewConfiguration(conf string) (*GeneralConfig, error) {
+func NewConfiguration(conf string) ([]*GeneralConfig, error) {
+
+	file, err := os.Stat(conf)
+
+	if err != nil {
+		log.Printf("Failed to identify status of conf")
+		return nil, err
+	}
+
+	switch mode := file.Mode(); {
+	case mode.IsDir():
+		listFile, err := filepath.Glob(conf + "*")
+		if err != nil {
+			return nil, err
+		}
+
+		for _, item := range listFile {
+			generalConf, err := GenerateFileToConf(conf + item)
+		}
+		return []*GeneralConfig{generalConf}, err
+	case mode.IsRegular():
+		generalConf, err := GenerateFileToConf(conf)
+		return []*GeneralConfig{generalConf}, err
+	}
+}
+
+func GenerateFileToConf(conf string) (*GeneralConfig, error) {
 	bytes, err := ioutil.ReadFile(conf)
 	if err != nil {
 		return nil, err
